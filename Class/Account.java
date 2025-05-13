f58eb51466c5bbc425af5ca0b3a82124ab972f5d
Abdus Salam Hidayatulloh <79743509+AbdusSalamHidayatulloh@users.noreply.github.com>
2025-05-13 14:45:30 +0700

    Add exercise attribure

--- Class/Account.java
+++ Class/Account.java
@@ -1,5 +1,3 @@
-package Account;
-
 
 public class Account {
     private String username, password;
--- Class/Exercise/WorkoutPlan.java
+++ Class/Exercise/WorkoutPlan.java
@@ -1,5 +1,5 @@
 package Exercise;
-
+import java.util.*;
 public class WorkoutPlan {
-
+    private ArrayList<Exercise> exercise;
 }
